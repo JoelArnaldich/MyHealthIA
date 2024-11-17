a439918012203125748124a8c880c29ef288d188
JoelArnaldich <joelarnaldich5@gmail.com>
2024-11-17 17:00:38 +0100

    pages acavar

---
+++ MyHealthAI/Views/Pages/GoalsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyHealthAI
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class GoalsPage : Page
+    {
+        public GoalsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

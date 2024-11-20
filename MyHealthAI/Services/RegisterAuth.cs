c8b524febc7f24fc04766f5a125c34de0930a00b
JoelArnaldich <joelarnaldich5@gmail.com>
2024-11-20 19:36:07 +0100

    registerOP

---
+++ MyHealthAI/Services/RegisterAuth.cs
@@ -0,0 +1,139 @@
+﻿using MyHealthAI.Migrations;
+using MyHealthAI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace MyHealthAI.Services
+{
+    public class RegisterAuth
+    {
+        public void register(String username, String password,String email,int height,double weight,int selectedOption,int? goalWeight)
+        {
+
+            String message = "";
+
+            // Crear una instancia del modelo User
+            User newUser = new User
+            {
+                Username = username,
+                Password = password,
+                Email = email,
+                Height = height,
+                Weight = weight,
+                ObjectiveID = selectedOption,
+                GoalWeight = goalWeight
+            };
+
+            try
+            {
+                // Crear una instancia del DbContext
+                using (var context = new AppDbContext())
+                {
+                    context.Users.Add(newUser); // Agregar el nuevo usuario
+                    context.SaveChanges(); // Guardar cambios en la base de datos
+                }
+
+                message = "Usuario guardado correctamente.";
+            }
+            catch (Exception ex)
+            {
+                message = $"Error al guardar los datos: {ex.Message}";
+
+            }
+
+        }
+
+        // Método para validar el correo (Gmail)
+        public bool IsValidEmail(string email)
+        {
+            // Expresión regular para validar un correo electrónico
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+            // Verificar que el correo no esté vacío y que cumpla con el patrón de la expresión regular
+            return !string.IsNullOrEmpty(email) && Regex.IsMatch(email, pattern);
+        }
+
+        // Método para validar la contraseña (al menos 8 caracteres, una mayúscula y un número)
+        public bool IsValidPassword(string password)
+        {
+            return !string.IsNullOrEmpty(password) && password.Length >= 8 &&
+                   password.Any(char.IsUpper) && password.Any(char.IsDigit);
+        }
+
+        public (bool, String) validate(String username, String password, String passwordc,String email, int height, double weight)
+        {
+            String message = "";
+            // Validar el nombre de usuario (al menos 3 caracteres)
+            if (username.Length < 3)
+            {
+               message  = "Username must be at least 3 characters.";
+                return (false,message);
+            }
+
+            // Validar la contraseña (debe tener al menos 8 caracteres, una mayúscula y un número)
+            if (!IsValidPassword(password))
+            {
+                message = "The password must have at least 8 characters, a capital letter and a number.";
+                return (false, message);
+            }
+
+            // Validaciones
+            if (password != passwordc)
+            {
+                message = "Password does not match.";
+                return (false, message);
+            }
+
+            // Validar el correo (debe ser un correo de Gmail válido)
+            if (!IsValidEmail(email))
+            {
+                message = "The email must be a valid Gmail address.";
+                return (false, message);
+            }
+
+            // Validar la altura (debe estar entre 0 y 4 metros)
+            if (height <= 10 || height > 400)
+            {
+                message = "The height must be between 10 and 400 cm.";
+                return (false, message);
+            }
+
+            // Validar el peso (debe estar entre 0 y 1000 kg)
+            if (weight <= 0 || weight > 1000)
+            {
+                message = "The weight must be between 0 and 1000 kg.";
+                return (false, message);
+            }
+
+            using (var context = new AppDbContext())
+            {
+                var existingUser = context.Users.FirstOrDefault(u => u.Username == username);
+                if (existingUser != null)
+                {
+                    message = "The username is already in use. Please choose another one.";
+                    return (false, message);
+                }
+            }
+
+            using (var context = new AppDbContext())
+            {
+                var existingUser = context.Users.FirstOrDefault(u => u.Email == email);
+                if (existingUser != null)
+                {
+                    message = "The email is already registered. Please choose another one.";
+                    return (false, message);
+                }
+            }
+
+
+
+            return (true, message);
+
+        }
+    }
+}
--- MyHealthAI/Views/RegisterView.xaml.cs
+++ MyHealthAI/Views/RegisterView.xaml.cs
@@ -2,7 +2,9 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
+using Microsoft.VisualBasic.ApplicationServices;
 using MyHealthAI.Models;
+using MyHealthAI.Services;
 
 namespace MyHealthAI
 {
@@ -40,63 +42,20 @@ namespace MyHealthAI
             {
                 goalWeight = g;
             }
+            RegisterAuth val = new RegisterAuth();
 
-            if (!validate(username, password, email, passwordc, height, weight))
+            (bool resultat, String missatge) = val.validate(username, password,passwordc,email,height,weight);
+          
+            if (!resultat)
             {
+                MessageBox.Show(missatge);
                 return;
 
             }
 
-            // Crear una instancia del modelo User
-            User newUser = new User
-            {
-                Username = username,
-                Password = password,
-                Email = email,
-                Height = height,
-                Weight = weight,
-                ObjectiveID = selectedOption,
-                GoalWeight = goalWeight
-            };
+            val.register(username,password,email,height,weight,selectedOption,goalWeight);
 
-            try
-            {
-                // Crear una instancia del DbContext
-                using (var context = new AppDbContext())
-                {
-                    context.Users.Add(newUser); // Agregar el nuevo usuario
-                    context.SaveChanges(); // Guardar cambios en la base de datos
-                }
-
-                MessageBox.Show("Usuario guardado correctamente.");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error al guardar los datos: {ex.Message}");
-
-                if (ex.InnerException != null)
-                {
-                    // Mostrar el mensaje de la InnerException
-                    MessageBox.Show($"Inner Exception: {ex.InnerException.Message}");
-
-                    // Mostrar más detalles si es necesario
-                    MessageBox.Show($"Inner Exception StackTrace: {ex.InnerException.StackTrace}");
-                }
-            }
-
-           
         }
-
-        /* 
-         
-         (bool resultat, String missatge) = autenica(user, password);
-
-        private (bool,String) autenica(String user,String password)
-        {
-            return (false, "invalit");
-        }
-
-        */
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
@@ -123,91 +82,6 @@ namespace MyHealthAI
             }
         }
 
-        // Método para validar el correo (Gmail)
-        private bool IsValidEmail(string email)
-        {
-            // Expresión regular para validar un correo electrónico
-            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
-
-            // Verificar que el correo no esté vacío y que cumpla con el patrón de la expresión regular
-            return !string.IsNullOrEmpty(email) && Regex.IsMatch(email, pattern);
-        }
-
-        // Método para validar la contraseña (al menos 8 caracteres, una mayúscula y un número)
-        private bool IsValidPassword(string password)
-        {
-            return !string.IsNullOrEmpty(password) && password.Length >= 8 &&
-                   password.Any(char.IsUpper) && password.Any(char.IsDigit);
-        }
-
-        private bool validate(String username, String password, String email, String passwordc, int height, double weight)
-        {
-            // Validar el nombre de usuario (al menos 3 caracteres)
-            if (username.Length < 3)
-            {
-                MessageBox.Show("Username must be at least 3 characters.");
-                return false;
-            }
-
-            // Validar la contraseña (debe tener al menos 8 caracteres, una mayúscula y un número)
-            if (!IsValidPassword(password))
-            {
-                MessageBox.Show("The password must have at least 8 characters, a capital letter and a number.");
-                return false;
-            }
-
-            // Validaciones
-            if (password != passwordc)
-            {
-                MessageBox.Show("Password does not match.");
-                return false;
-            }
-
-            // Validar el correo (debe ser un correo de Gmail válido)
-            if (!IsValidEmail(email))
-            {
-                MessageBox.Show("The email must be a valid Gmail address.");
-                return false;
-            }
-
-            // Validar la altura (debe estar entre 0 y 4 metros)
-            if (height <= 10 || height > 400)
-            {
-                MessageBox.Show("The height must be between 10 and 400 cm.");
-                return false;
-            }
-
-            // Validar el peso (debe estar entre 0 y 1000 kg)
-            if (weight <= 0 || weight > 1000)
-            {
-                MessageBox.Show("The weight must be between 0 and 1000 kg.");
-                return false;
-            }
-
-            using (var context = new AppDbContext())
-            {
-                var existingUser = context.Users.FirstOrDefault(u => u.Username == username);
-                if (existingUser != null)
-                {
-                    MessageBox.Show("The username is already in use. Please choose another one.");
-                    return false;
-                }
-            }
-
-            using (var context = new AppDbContext())
-            {
-                var existingUser = context.Users.FirstOrDefault(u => u.Email == email);
-                if (existingUser != null)
-                {
-                    MessageBox.Show("The email is already registered. Please choose another one.");
-                    return false;
-                }
-            }
-
-
-
-            return true;
-
-        }
+       
     }
 }
